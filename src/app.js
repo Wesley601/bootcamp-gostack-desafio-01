a73bc4b971649ea2bcc9f1e5e7ebaab9058ed7c4
wesley601 <wesleypassos601@gmail.com>
2020-08-17 21:36:57 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,60 @@ app.use(cors());
 
 const repositories = [];
 
+function validateIndex(request, response, next)
+{
+  const { id } = request.params;
+
+  index = repositories.findIndex(value => value.id === id);
+
+  if (index < 0) {
+    return response.status(400).json({message: 'not found'});
+  }
+
+  request.RepositoryIndex = index;
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+  repositories.push(repository);
+  return response.status(200).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateIndex, (request, response) => {
+  repository = { title, url, techs } = request.body
+
+  repositories[request.RepositoryIndex] = {
+    id: repositories[request.RepositoryIndex].id,
+    title,
+    url,
+    techs,
+    likes: repositories[request.RepositoryIndex].likes
+  }
+  return response.json(repositories[request.RepositoryIndex]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateIndex, (request, response) => {
+  repositories.splice(request.RepositoryIndex, 1);
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateIndex, (request, response) => {
+  repositories[request.RepositoryIndex].likes++;
+  return response.json(repositories[request.RepositoryIndex]);
+
 });
 
 module.exports = app;
